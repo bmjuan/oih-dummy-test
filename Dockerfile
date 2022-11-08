cc2a472ac030d5a3b30829d71801df55e8b13477
Beatriz Martin <beatriz.martin@new-work.se>
2022-11-08 13:19:00 +0100

    feat: first test to add composed flows

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:16-alpine
+
+RUN apk add --no-cache libc6-compat
+
+
+WORKDIR /usr/src/app
+
+COPY package.json /usr/src/app
+
+RUN npm install --production
+
+COPY . /usr/src/app
+
+RUN chown -R node:node .
+
+USER node
+
+ENTRYPOINT ["npm", "start"]
---
+++ test/utils.test.js
@@ -0,0 +1,76 @@
+/* eslint no-unused-expressions: "off" */
+
+const { expect } = require('chai');
+const nock = require('nock');
+const { getToken, getObjects, upsertObject } = require('./../lib/utils/helpers');
+
+describe.skip('Helpers', () => {
+  it('should perform a login', async () => {
+    nock('https://api.example.com')
+      .post('/login', { username: 'ExampleUser', password: 'ExamplePassword' })
+      .reply(200, { token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' });
+
+
+    const cfg = {
+      username: 'ExampleUser',
+      password: 'ExamplePassword',
+    };
+    const token = await getToken(cfg);
+    expect(token).to.equal('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9');
+  });
+
+  it('should get Objects, filtered by timestamp', async () => {
+    nock('https://api.example.com', { reqheaders: { authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' } })
+      .get('/')
+      .reply(200, [
+        {
+          firstName: 'Jane',
+          lastName: 'Doe',
+          lastUpdate: '2020-10-26T15:44:10+0000',
+        },
+        {
+          firstName: 'Somebody',
+          lastName: 'Else',
+          lastUpdate: '2019-10-26T15:44:10+0000',
+        },
+        {
+          firstName: 'Nobody',
+          middleName: 'of',
+          lastName: 'Note',
+          lastUpdate: '2018-10-26T15:44:10+0000',
+        },
+      ]);
+
+    const objects = await getObjects('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9', { lastUpdate: '2019-05-12T15:44:10+0000' });
+
+    expect(objects).to.have.lengthOf(2);
+  });
+
+  it('should dynamically insert an object', async () => {
+    nock('https://api.example.com', { reqheaders: { authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' } })
+      .post('/', { firstName: 'New', lastName: 'Entry' })
+      .reply(201, { id: '4711' });
+
+
+    const { success, responseId } = await upsertObject({ firstName: 'New', lastName: 'Entry' }, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9');
+
+    expect(success).to.be.true;
+    expect(responseId).to.equal('4711');
+  });
+
+  it('should dynamically update an object', async () => {
+    nock('https://api.example.com', { reqheaders: { authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' } })
+      .get('/4711')
+      .reply(200, { id: '4711' });
+
+    nock('https://api.example.com', { reqheaders: { authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' } })
+      .put('/4711', { firstName: 'Updated' })
+      .reply(200, { id: '4711' });
+
+
+    const { success, responseId } = await upsertObject({ firstName: 'Updated' }, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9', '4711');
+
+    expect(success).to.be.true;
+    expect(responseId).to.equal('4711');
+  });
+});
